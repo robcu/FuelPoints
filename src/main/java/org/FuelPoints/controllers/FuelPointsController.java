bc87850dafa20f12d6cbb9a7e5eeba8844b31bc3
robcu <robculclasure@Robs-MacBook-Pro.local>
2017-04-05 17:23:13 -0500

    Add class JsonUser. Refactor /login

--- src/main/java/org/FuelPoints/controllers/FuelPointsController.java
+++ src/main/java/org/FuelPoints/controllers/FuelPointsController.java
@@ -1,17 +1,23 @@
 package org.FuelPoints.controllers;
 
+import org.FuelPoints.entities.JsonUser;
 import org.FuelPoints.entities.User;
+import org.FuelPoints.entities.Vehicle;
 import org.FuelPoints.services.UserRepository;
 import org.FuelPoints.services.VehicleRepository;
 import org.FuelPoints.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import jodd.json.JsonParser;
+
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FuelPointsController {
@@ -22,33 +28,66 @@ public class FuelPointsController {
 
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public User login(HttpServletResponse response, String username, String password) throws Exception {
-        User user = users.findFirstByName(username);
+    public User login(HttpServletResponse response, @RequestBody String body) throws Exception {
+
+
+        JsonParser p = new JsonParser();
+        JsonUser jsonUser = p.parse(body, JsonUser.class);
+
+        User user = users.findFirstByName(jsonUser.getName());
         if (user == null) {
             response.sendError(401, "Account does not exist.");
-        } else if (!user.verifyPassword(password)) {
-            response.sendError(401, "Invalid credentials.");
+        } else if (!user.verifyPassword(jsonUser.getPassword())) {
+            response.sendError(401, "Invalid credentials");
         }
         return user;
     }
 
-    @RequestMapping(path = "/logout", method = RequestMethod.POST)
-    public void logout(HttpServletResponse response) {
-
-    }
-
-
-    @RequestMapping(path = "/register", method = RequestMethod.POST)
-    public void register(HttpServletResponse response, String username, String password) throws IOException, PasswordStorage.CannotPerformOperationException {
-        User user = users.findFirstByName(username);
-        if (user != null) {
-            response.sendError(401, "Username is not available.");
-        } else {
-            users.save(new User(username, password));
-            response.sendError(201, "User successfully created.");
-        }
-    }
-
+//    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+//    public void logout(HttpServletResponse response) {
+//
+//    }
+//
+//
+//    @RequestMapping(path = "/register", method = RequestMethod.POST)
+//    public void register(HttpServletResponse response, @RequestBody String body) throws IOException, PasswordStorage.CannotPerformOperationException {
+//        User user = users.findFirstByName(username);
+//        if (user != null) {
+//            response.sendError(401, "Username is not available.");
+//        } else {
+//            users.save(new User(username, password));
+//            response.sendError(201, "User successfully created.");
+//        }
+//    }
+//
+//    @RequestMapping(path = "/add_vehicle", method = RequestMethod.POST)
+//    public void addVehicle(HttpServletResponse response, @RequestBody String body) throws IOException {
+//        vehicles.save(new Vehicle(make, model, year, user));
+//        response.sendError(201, "Vehicle added.");
+//    }
+//
+//    @RequestMapping(path = "/delete_vehicle", method = RequestMethod.POST)
+//    public void deleteVehicle(HttpServletResponse response, @RequestBody String body) throws IOException {
+//        vehicles.delete(id);
+//        response.sendError(200, "Vehicle deleted");
+//    }
+//    @RequestMapping(path = "/change_username", method = RequestMethod.POST)
+//    public void changeUsername(HttpServletResponse response, @RequestBody String body){
+//
+//    }
+//
+//
+//    @RequestMapping(path = "/change_password", method = RequestMethod.POST)
+//    public void changePassword(HttpServletResponse response, @RequestBody String body) throws PasswordStorage.CannotPerformOperationException, PasswordStorage.InvalidHashException, IOException {
+//        User user = users.findFirstByName(username);
+//        if(user.verifyPassword(password)){
+//            user.setPassword(newPassword);
+//            users.save(user);
+//            response.sendError(200, "Password updated.");
+//        } else{
+//            response.sendError(401, "Invalid credentials");
+//        }
+//    }
 
 }
 
--- src/main/java/org/FuelPoints/entities/Vehicle.java
+++ src/main/java/org/FuelPoints/entities/Vehicle.java
@@ -7,6 +7,9 @@ import javax.persistence.*;
 public class Vehicle {
     @Id
     @GeneratedValue
+    Integer id;
+
+    @Column
     String make;
 
     @Column
